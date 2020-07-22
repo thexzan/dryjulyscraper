7fa7b15d63e8b532d6a0893d8c545ef0a9de5399
Xzan <hello@thexzan.com>
2020-07-22 20:34:22 +0700

    remove json2csv to manually append new item

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const scraper = require("./scraper");
-const { Parser } = require("json2csv");
 const fs = require("fs");
 
 (async () => {
   const url = "https://www.dryjuly.com/";
 
-  let data = await scraper.getData(url);
+  const data = await scraper.getData(url);
   // console.log(data);
-  try {
-    const fields = ["time", "people", "total"];
-    const parser = new Parser(fields);
-    const csv = parser.parse(data);
-    console.log(csv);
-    fs.writeFileSync("data.csv", csv);
-    debugger;
-  } catch (err) {
-    console.error(err);
-  }
+  const data2csv = `\n"${data.time}","${data.people}","${data.total}"`;
+  fs.appendFileSync("data.csv", data2csv);
 })();
