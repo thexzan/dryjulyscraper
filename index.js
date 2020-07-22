fce03998b9ec12a66866baf08eb7d94b47c2a18e
Xzan <hello@thexzan.com>
2020-07-22 19:44:44 +0700

    add json2csv

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const scraper = require("./scraper");
+const { Parser } = require("json2csv");
+const fs = require("fs");
 
 (async () => {
   const url = "https://www.dryjuly.com/";
 
   let data = await scraper.getData(url);
-  console.log(data);
+  // console.log(data);
+  try {
+    const fields = ["time", "people", "total"];
+    const parser = new Parser(fields);
+    const csv = parser.parse(data);
+    console.log(csv);
+    fs.writeFileSync("data.csv", csv);
+    debugger;
+  } catch (err) {
+    console.error(err);
+  }
 })();
